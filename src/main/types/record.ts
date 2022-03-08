de2516df1c57d03276267909753e2245f1aa34d5
chang tzu lin <coooo77@hotmail.com>
2022-03-09 00:01:36 +0800

    [refactor] Record Setting Type

--- src/main/types/record.ts
+++ src/main/types/record.ts
@@ -1,54 +1,51 @@
-export interface ICheckTypeOption {
-  isActive: boolean
-  targetType: string[]
-}
+export type IRecordType = "vod" | "stream";
 
-export type recordType =
-  | 'vod'
-  | 'stream'
-
-export type recordMode =
-  | 'queue'
-  | 'manual'
-  | 'offLine'
-  | 'timeZone'
-  | 'countDown'
-
-export interface IRecordVodOption {
-  // disable record stream while vod is available
-  isStopRecordStream: boolean
-  // enable record stream while vod is not available
-  getStreamIfNoVod: boolean
-  mode: recordMode
-  countDownInMinutes: number
-  timeZone: [number, number, number]
-}
-
-export interface ICheckStreamContentType {
-  enable: boolean
-  targetGameNames: string[]
-}
+export type IRecordMode =
+  | "queue"
+  | "manual"
+  | "offLine"
+  | "timeZone"
+  | "countDown";
 
 export interface IUserStatus {
-  isOnline: boolean
-  isRecording: boolean
-  streamStartedAt?: string
+  isOnline: boolean;
+  isRecording: boolean;
+  streamStartedAt?: string;
 }
 
 export interface IRecordSetting {
-  enableRecord: boolean
-  enableNotify: boolean
-  vod: IRecordVodOption
-  recordType: recordType[]
-  offlineTimesToCheck?: number
-  checkStreamContentType: ICheckStreamContentType
-  fileNameTemplate: string
+  /** enable record stream or vod */
+  enableRecord: boolean;
+  /** enable online notify */
+  enableNotify: boolean;
+  /** disable record stream while vod is available */
+  vodIsStopRecordStream: boolean;
+  /** enable record stream while vod is not available */
+  vodGetStreamIfNoVod: boolean;
+  /** queue、manual、offLine、timeZone、countDown */
+  vodMode: IRecordMode;
+  /** setting for record mode - countDown */
+  vodCountDownInMinutes: number;
+  /** [hour, minute, second] */
+  vodTimeZone: [number, number, number];
+  /** e.g. {channel}-TwitchVOD-{date}-{duration} */
+  vodFileNameTemplate: string;
+  /** vod | stream */
+  recordType: IRecordType[];
+  /** offline check */
+  offlineTimesToCheck?: number;
+  /** record stream for explicit game names only */
+  checkStreamContentTypeEnable: boolean;
+  /** e.g. Art;Just Chatting; */
+  checkStreamContentTypeTargetGameNames: string;
+  /** e.g. {channel}-TwitchLive-{date} */
+  fileNameTemplate: string;
 }
 
 export interface IGetUsersResponse {
-  id: string
-  login: string
-  display_name: string
-  profile_image_url: string
-  offline_image_url: string
+  id: string;
+  login: string;
+  display_name: string;
+  profile_image_url: string;
+  offline_image_url: string;
 }
